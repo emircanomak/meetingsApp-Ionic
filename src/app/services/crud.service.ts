f012f0a33153534b38a970901a0f116df156511b
Emircan Omak <omakemircan@hotmail.com>
2023-05-28 23:17:42 +0300

    update service

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { IAddMeetingModel } from '../models/addMeetingModel';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ICreateMeetingModel } from '../models/createMeetingModel';
+import { IUpdateMeetingModel } from '../models/updateMeetingModel';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +16,12 @@ export class CrudService {
   
   addMeeting(value:any){
     return this.httpClient.post(this.apiUrl, value);
+  }
+  getMeetingById(id:number): Observable<ICreateMeetingModel> {
+    return this.httpClient.get<ICreateMeetingModel>(this.apiUrl + '/' + id)
+  }
+  meetingUpdate(id:number, data:IUpdateMeetingModel): Observable<IUpdateMeetingModel>{
+    return this.httpClient.put<IUpdateMeetingModel>(this.apiUrl + "/" + id, data)
   }
   
 }
