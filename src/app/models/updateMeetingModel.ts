9115b493c4258d7bb4459eaefa1c43a89c028b4f
Emircan Omak <omakemircan@hotmail.com>
2023-05-28 23:14:40 +0300

    add update meeting model

---
+++ src/app/models/updateMeetingModel.ts
@@ -0,0 +1,10 @@
+import { Time } from "@angular/common";
+
+export interface IUpdateMeetingModel {
+    customerName:string;
+    customerLastName:string;
+    dateOfMeeting:Date;
+    timeOfMeeting:number;
+    service:string;
+    
+}
